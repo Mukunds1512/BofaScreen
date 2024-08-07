74cd83c47ee917b1d4137cc20cb086e2b67dd153
Mukunds1512 <mukunds1512@gmail.com>
2024-08-07 12:11:03 -0400

    Initial commit implement ScrollableStackView

---
+++ BofaScreen/DropDownView.swift
@@ -0,0 +1,15 @@
+//
+//  DropDownView.swift
+//  BofaScreen
+//
+//  Created by Mukund Sharma on 8/7/24.
+//
+
+import Foundation
+import UIKit
+
+class DropDownView: UIView {
+    
+    private let label = UILabel()
+    
+}
---
+++ BofaScreen/ScrollableStackView.swift
@@ -0,0 +1,55 @@
+//
+//  ScrollableStackView.swift
+//  BofaScreen
+//
+//  Created by Mukund Sharma on 8/7/24.
+//
+
+import Foundation
+import UIKit
+
+class ScrollableStackView: UIView {
+    private let scrollView = UIScrollView()
+    private let stackView = UIStackView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(scrollView)
+        
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+        ])
+    }
+    
+    func addArrangedSubview(_ view: UIView){
+        stackView.addArrangedSubview(view)
+    }
+    
+}
+    
+
