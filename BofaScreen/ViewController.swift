5615cf7ffe5872772eaac271cb3f4012999929db
Mukunds1512 <mukunds1512@gmail.com>
2024-08-07 14:01:33 -0400

    Setup for ScrollableStackView

--- BofaScreen/ViewController.swift
+++ BofaScreen/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class BofaScreenViewController: UIViewController {
-
+    
+    private let scrollableStackView = ScrollableStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+    }
+    
+    private func setupScrollableStackView() {
+        
+        scrollableStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollableStackView)
+        
+        NSLayoutConstraint.activate([
+            scrollableStackView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scrollableStackView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            scrollableStackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            scrollableStackView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+        ])
     }
 
 
